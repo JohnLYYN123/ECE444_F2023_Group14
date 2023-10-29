83c20a2b11df347aa25fae84d07860e380124059
Eric Zheng <eric.z1@outlook.com>
2023-10-29 18:00:23 -0400

    Adding function and tests for comment section

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
 from routes.user_authentication_system import user
+from routes.detailed_system import detail
+
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(user)
+    app.register_blueprint(detail)
     return app
---
+++ backend/routes/detailed_system.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify, render_template, request, redirect, url_for
+
+detail = Blueprint("detail",__name__, url_prefix="/detail")
+
+@detail.route("/", methods=['POST'])
+def display():
+    # page not built yet, return to index with nothing for now
+    return render_template("index.html")
+
+@detail.route("/add", methods=['POST','GET'])
+def add_entry():
+    """Adds new comment to the comment section."""
+    data = request.get_json()
+    comment_title = data.get('Title')
+    comment_content = data.get('Content')
+
+    if (comment_title) and (comment_content):
+        return redirect(url_for("detail.display"))
+    
+    return jsonify({'message': 'Empty field not allowed'}), 406
+    
+    
+    
+    
