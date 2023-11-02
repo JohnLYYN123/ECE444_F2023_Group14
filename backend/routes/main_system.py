8b6fb2bbe3579118631d064de890f503404f51e8
JohnLYYN123 <96747053+JohnLYYN123@users.noreply.github.com>
2023-11-02 14:56:52 -0400

    Organize main system

--- backend/routes/main_system.py
+++ backend/routes/main_system.py
@@ -8,8 +8,23 @@ from sqlalchemy import text
 
 main_sys = Blueprint("main_sys", __name__, url_prefix="/main_sys")
 
-# this is filter event implementation
-# test
+
+@main_sys.route('/filter', methods=["GET"])
+def filter_event():
+    filter_title = request.args.get('title')
+    if isinstance(filter_title, str) is False:
+        return jsonify({"code": 401, "msg": "Illegal input", "data": []}), 401
+
+    if not filter_title:
+        return jsonify({"code": 200, "msg": "No filter applied", "data": []}), 200
+
+    if filter_title not in ["sport", "art", "travel", "cooking"]:
+        return jsonify({"code": 200, "msg": "filter does not exist", "data": []}), 200
+    # filter_list = filter_title.split(",")
+    data = filter_event_impl(filter_title)
+    return jsonify({"code": 200, "msg": "OK", "data": data}), 200
+
+
 def filter_event_impl(filter_list):
     from models.event_filter import EventFilerDB # noqa
     from models.event_info import EventInfoDB # noqa
@@ -33,19 +48,37 @@ def filter_event_impl(filter_list):
     return result
 
 
-def insert_event_impl(event_id, filter_name):
-    # from models.Event_model import db  # noqa
-    # sql_conn = db
-    from models.event_filter import EventFilerDB  # noqa
+@main_sys.route('/add_filter', methods=['GET'])
+def add_event_filter():
+    event_id = request.args.get('event_id')
+    filter_name = request.args.get('filter_name')
+
+    # from .Models.main_system_model import MainSysModel
+    status, e = insert_event_impl(event_id, filter_name)
+    if status is False:
+        return jsonify({"code": 200, "msg": "INSERTION FAILED", "data": e}), 200
+
+    return jsonify({"code": 200, "msg": "INSERTED", "data": []}), 200
+
+
+def insert_new_event(event_id, name, desc, organizer):
+    from models.event_info import EventInfoDB  # noqa
     from backend import db
-    new_event_filter = EventFilerDB(event_id, filter_name)
+
+    new_event_info = EventInfoDB(event_id, name, desc, organizer)
     try:
-        db.session.add(new_event_filter)
+        db.session.add(new_event_info)
         db.session.commit()
     except Exception as e:
         return False, str(e)
 
     return True, ""
+
+
+@main_sys.route('/view_events')
+def view_event():
+    data = view_event_impl()
+    return jsonify({"code": 200, "msg": "OK", "data": data}), 200
 
 
 def view_event_impl():
@@ -62,54 +95,6 @@ def view_event_impl():
     return result
 
 
-def insert_new_event(event_id, name, desc, organizer):
-    from models.event_info import EventInfoDB  # noqa
-    from backend import db
-
-    new_event_info = EventInfoDB(event_id, name, desc, organizer)
-    try:
-        db.session.add(new_event_info)
-        db.session.commit()
-    except Exception as e:
-        return False, str(e)
-
-    return True, ""
-
-@main_sys.route('/filter', methods=["GET"])
-def filter_event():
-    filter_title = request.args.get('title')
-    if isinstance(filter_title, str) is False:
-        return jsonify({"code": 401, "msg": "Illegal input", "data": []}), 401
-
-    if not filter_title:
-        return jsonify({"code": 200, "msg": "No filter applied", "data": []}), 200
-
-    if filter_title not in ["sport", "art", "travel", "cooking"]:
-        return jsonify({"code": 200, "msg": "filter does not exist", "data": []}), 200
-    # filter_list = filter_title.split(",")
-    data = filter_event_impl(filter_title)
-    return jsonify({"code": 200, "msg": "OK", "data": data}), 200
-
-@main_sys.route('/add_filter', methods=['GET'])
-def add_event_filter():
-    event_id = request.args.get('event_id')
-    filter_name = request.args.get('filter_name')
-
-    # from .Models.main_system_model import MainSysModel
-    status, e = insert_event_impl(event_id, filter_name)
-    if status is False:
-        return jsonify({"code": 200, "msg": "INSERTION FAILED", "data": e}), 200
-
-    return jsonify({"code": 200, "msg": "INSERTED", "data": []}), 200
-
-
-
-@main_sys.route('/view_events')
-def view_event():
-    data = view_event_impl()
-    return jsonify({"code": 200, "msg": "OK", "data": data}), 200
-
-
 @main_sys.route("/add_event", methods=['GET'])
 def add_event_info():
     event_id = request.args.get('event_id')
@@ -122,4 +107,27 @@ def add_event_info():
         return jsonify({"code": 200, "msg": "INSERTION FAILED", "data": e}), 200
 
     return jsonify({"code": 200, "msg": "INSERTED", "data": []}), 200
+
+
+def insert_event_impl(event_id, filter_name):
+    # from models.Event_model import db  # noqa
+    # sql_conn = db
+    from models.event_filter import EventFilerDB  # noqa
+    from backend import db
+    new_event_filter = EventFilerDB(event_id, filter_name)
+    try:
+        db.session.add(new_event_filter)
+        db.session.commit()
+    except Exception as e:
+        return False, str(e)
+
+    return True, ""
+
+
+
+
+
+
+
+
 
