87148cf188bbc3de2c30f990f7ac9218b5240aa3
Tianrui-Luo <77422312+Tianrui-Luo@users.noreply.github.com>
2023-11-11 10:08:09 -0500

    Update enrollment_cart_system.py

--- backend/routes/enrollment_cart_system.py
+++ backend/routes/enrollment_cart_system.py
@@ -32,3 +32,26 @@ def requires_auth(f):
         return jsonify(message="Authentication is required to access this resource"), 401
 
     return decorated
+
+
+# Endpoint to get enrolled events based on the type (upcoming/past)
+@enrollment.route('/enrollment/<int:id>?type=${viewing_type}', methods=['GET'])
+def get_enrolled_events():
+    from models.user_enroll_event_model import UserEnrollEventModel
+    from backend import db
+    from backend.models.user_model import UserModel
+    user = UserModel.query.filter_by(
+        user_id=g.current_user["user_id"]).first()
+    
+    enrollments = UserEnrollEventModel.query.filter_by(user_id=user).all()
+
+    viewing_type = request.args.get('type', 'upcoming')
+
+    now = datetime.now()
+
+    if viewing_type == 'upcoming':
+        filtered_events = [event for event in enrollments if  datetime.strptime(enrollments, '%Y-%m-%d') >= now]
+    else:
+        filtered_events = [event for event in enrollments if  datetime.strptime(enrollments, '%Y-%m-%d') < now]
+
+    return jsonify(filtered_events)
