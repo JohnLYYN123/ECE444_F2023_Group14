e6c79e6c7006f82e4cc3a3ab7354da66899f9e75
Tianrui-Luo <77422312+Tianrui-Luo@users.noreply.github.com>
2023-11-10 22:51:35 -0500

    Update enrollment_cart_system.py by rewriting the enrollment func and add the distinguishing upcoming/past events func

--- backend/routes/enrollment_cart_system.py
+++ backend/routes/enrollment_cart_system.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, flash, jsonify, render_template, request, g
+from flask import Flask, request, jsonify, g
+from datetime import datetime
 from functools import wraps
 from itsdangerous import SignatureExpired, BadSignature
 from itsdangerous import URLSafeTimedSerializer as Serializer
@@ -34,41 +35,25 @@ def requires_auth(f):
 
     return decorated
 
-@enrollment.route('/enrollment/<int:id>/', methods=['GET'])
-# Show the enrollment events
-def show_event():
-    from routes.main_system import view_detail
+
+# Endpoint to get enrolled events based on the type (upcoming/past)
+@enrollment.route('/enrollment/<int:id>?type=${viewing_type}', methods=['GET'])
+def get_enrolled_events():
     from models.user_enroll_event_model import UserEnrollEventModel
     from backend import db
-
     from backend.models.user_model import UserModel
     user = UserModel.query.filter_by(
         user_id=g.current_user["user_id"]).first()
     
     enrollments = UserEnrollEventModel.query.filter_by(user_id=user).all()
-    for enrollment in enrollments:
-        enrolled_eventid = [enrollment.event_id]
-        EnrolledEvent = view_detail(enrolled_eventid)
-        return jsonify({"code": 200, "msg": "OK", "data": EnrolledEvent}), 200
-    
 
-@enrollment.route("/register/<int:id>/", methods=['POST', 'GET'])
-def enrol(id):
-    from models.user_enroll_event_model import UserEnrollEventModel
-    from backend import db
+    viewing_type = request.args.get('type', 'upcoming')
 
-    from backend.models.user_model import UserModel
-    user = UserModel.query.filter_by(
-        user_id=g.current_user["user_id"]).first()
+    now = datetime.now()
 
-    from backend.models.event_info_model import EventInfoModel
-    current_event = EventInfoModel.query.filter_by(event_id=id).first()
+    if viewing_type == 'upcoming':
+        filtered_events = [event for event in enrollments if  datetime.strptime(enrollments, '%Y-%m-%d') >= now]
+    else:
+        filtered_events = [event for event in enrollments if  datetime.strptime(enrollments, '%Y-%m-%d') < now]
 
-    new_enrollment = UserEnrollEventModel(user_id=user, event_id=current_event)
-    try:
-        db.session.add(new_enrollment)
-        db.session.commit()
-    except Exception as e:
-        return False, str(e)
-
-    return True, ""
+    return jsonify(filtered_events)
