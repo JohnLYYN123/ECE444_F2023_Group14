5fee4f055714ef6348c9092274ea7a8f21af3588
Eric Zheng <eric.z1@outlook.com>
2023-11-13 23:03:15 +0800

    Fixed minor error in test, works properly now

--- backend/tests/unit_test/test_review_comment_model.py
+++ backend/tests/unit_test/test_review_comment_model.py
@@ -6,7 +6,7 @@ class TestReviewRatingModel(unittest.TestCase):
         self.host_event_mock = [
             {
                 "event_id": 1,
-                "review_user": 1,
+                "review_user": 5,
                 "rating": 4,
                 "review_comment": "I really enjoyed it!",
             },
@@ -15,13 +15,7 @@ class TestReviewRatingModel(unittest.TestCase):
     # Test if ReviewRatingModel constructs correctly
     def test_model_init(self):
         for comment_data in self.host_event_mock:
-            mock_model = ReviewRatingModel(
-                event_id=comment_data['event_id'],
-                review_user=comment_data['review_user'],
-                rating=comment_data['rating'],
-                review_comment=comment_data['review_comment'],
-
-            )
+            mock_model = ReviewRatingModel(comment_data)
             self.assertEqual(mock_model.review_comment, comment_data['review_comment'])
             self.assertEqual(mock_model.rating, comment_data['rating'])
 
