2d65932db82dde3a38796ab17107b78e34f24d4b
Yoko <yokofenglee@gmail.com>
2023-11-14 12:14:36 -0500

    update tdd test cases to avoid create new db

--- backend/tests/tdd/test_user_authentication.py
+++ backend/tests/tdd/test_user_authentication.py
@@ -1,76 +1,31 @@
 import pytest
-from backend import app, db, bcrypt
+from backend import app, bcrypt
 from backend.models.user_model import UserModel
 
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+def test_register_existing_user():
     client = app.test_client()
-
-    with app.app_context():
-        db.create_all()
-        yield client
-        db.drop_all()
-
-
-def test_register_existing_user(client):
-    # Create a user before testing registration of an existing user
-    existing_user = UserModel(
-        username='existinguser',
-        uoft_email='existinguser@utoronto.ca',
-        password_hash=bcrypt.generate_password_hash(
-            'existingpassword').decode('utf-8'),
-        uoft_student_id='54321',
-        first_name='Existing',
-        last_name='User',
-        department='Example Department',
-        enrolled_time='2023-11-10',
-        organizational_role=False
-    )
-    with app.app_context():
-        db.session.add(existing_user)
-        db.session.commit()
-
     data = {
-        'username': 'existinguser',
-        'uoftEmail': 'existinguser@utoronto.ca',
-        'password': 'existingpassword',
-        'uoftStudentId': '54321',
-        'firstName': 'Existing',
-        'lastName': 'User',
-        'enrolledTime': '2023-11-10',
-        'organizationalRole': False
+        "username": "guitar hero",
+        "uoftEmail": "guitar.hero@mail.utoronto.ca",
+        "uoftStudentId": "1004140140",
+        "firstName": "guitar",
+        "lastName": "hero",
+        "department": "Music",
+        "enrolled_time": "2023",
+        "password": bcrypt.generate_password_hash("asdf").decode('utf-8'),
+        "organizational_role": True
     }
 
     response = client.post('/user/register', json=data)
-
-    assert response.status_code == 401
     assert 'Username already exists' in response.json['error']
 
 
-def test_login_successful(client):
-    # Create a user before testing login
-    user = UserModel(
-        username='testuser',
-        uoft_email='testuser@utoronto.ca',
-        password_hash=bcrypt.generate_password_hash(
-            'testpassword').decode('utf-8'),
-        uoft_student_id='12345',
-        first_name='Test',
-        last_name='User',
-        department='Example Department',
-        enrolled_time='2023-11-10',
-        organizational_role=False
-    )
-    with app.app_context():
-        db.session.add(user)
-        db.session.commit()
-
+def test_login_successful():
+    client = app.test_client()
     data = {
-        'username': 'testuser',
-        'password': 'testpassword'
+        'username': 'guitar hero',
+        'password': 'asdf'
     }
 
     response = client.post('/user/login', json=data)
@@ -79,42 +34,23 @@ def test_login_successful(client):
     assert 'token' in response.json
 
 
-def test_login_wrong_password(client):
-    # Create a user before testing login
-    user = UserModel(
-        username='testuser',
-        uoft_email='testuser@utoronto.ca',
-        password_hash=bcrypt.generate_password_hash(
-            'testpassword').decode('utf-8'),
-        uoft_student_id='12345',
-        first_name='Test',
-        last_name='User',
-        department='Example Department',
-        enrolled_time='2023-11-10',
-        organizational_role=False
-    )
-    with app.app_context():
-        db.session.add(user)
-        db.session.commit()
-
+def test_login_wrong_password():
+    client = app.test_client()
     data = {
-        'username': 'testuser',
+        'username': 'guitar hero',
         'password': 'wrongpassword'
     }
-
     response = client.post('/user/login', json=data)
-
     assert response.status_code == 401
     assert 'Wrong Password - Try Again!' in response.json['error']
 
 
-def test_login_unauthorized_access(client):
+def test_login_unauthorized_access():
+    client = app.test_client()
     data = {
         'username': 'nonexistentuser',
         'password': 'testpassword'
     }
-
     response = client.post('/user/login', json=data)
-
     assert response.status_code == 409
     assert 'Please register an account first!' in response.json['error']
